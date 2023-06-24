0be667efe4f1a06b7f64cd2b47a1132b3bc94f9b
XTroll <xtroll@inbox.ru>
2023-06-24 17:13:45 +1000

    C#_Homework_3

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.WriteLine("Введите пятизначное число: ");
+int[] array = new int [5];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.Read());
+}
+
+if (array[0] == array[4] && array[1] == array[3])
+{
+    System.Console.WriteLine("Число является палиндромом");
+}
+else System.Console.WriteLine("Число не является палиндромом");
---
+++ Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+System.Console.Write("Введите координаты X,Y,Z первой точки через запятую: ");
+int[] aPoint = Array.ConvertAll(Console.ReadLine()!.Split(","), int.Parse);
+
+System.Console.Write("Введите координаты X,Y,Z второй точки через запятую: ");
+int[] bPoint = Array.ConvertAll(Console.ReadLine()!.Split(","), int.Parse);
+
+System.Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow((bPoint[0] - aPoint[0]), 2) + Math.Pow((bPoint[1] - aPoint [1]), 2) + Math.Pow((bPoint[2] - aPoint[2]), 2)), 2));
